b8bf3b7d9df1b5c0f7be6a6691bcebb0bf28c2d1
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2020-11-27 06:32:59 +0700

    up

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+var login = document.getElementById('login');
+var hideLogin = document.querySelector('.form-login');
+var regis = document.getElementById('register');
+var hideRegis = document.querySelector('.form-regis');
+var acc = document.getElementById('account');
+var modal = document.querySelector('.modal');
+var delCartItem1 = document.getElementById('cart-item_1');
+var delCartItem2 = document.getElementById('cart-item_2');
+var delCartItem3 = document.getElementById('cart-item_3');
+let numberItem = 3;
+var numItem = document.querySelector('.header__cart-notice');
+numItem.innerHTML = numberItem;
+// var checkNumItem = numberItem - 1;
+
+function regisAcc(){
+    regis.style.display = 'block';
+    modal.style.display = 'flex';
+};
+
+function loginAcc(){
+    login.style.display = 'block';
+    modal.style.display = 'flex';
+};
+
+function switchLogin(){
+    regis.style.display = 'none';
+    login.style.display = 'block';
+};
+
+function switchRegister(){
+    regis.style.display = 'block';
+    login.style.display = 'none';
+};
+
+function exitModal(){
+    modal.style.display = 'none';
+    regis.style.display = 'none';
+    login.style.display = 'none';
+};
+
+function successAcc(){
+    hideLogin.style.display = 'none';
+    hideRegis.style.display = 'none';
+    modal.style.display = 'none';
+    acc.style.display = 'inline-flex';
+};
+
+function logOut(){
+    acc.style.display = 'none';
+    hideLogin.style.display = 'block';
+    hideRegis.style.display = 'block';
+};
+
+function deleteItem1(){
+    delCartItem1.style.display = 'none';
+    numItem.innerHTML = numberItem - 1;
+    
+};
+
+function deleteItem2(){
+    delCartItem2.style.display = 'none';
+    numItem.innerHTML = numberItem - 2;
+};
+
+function deleteItem3(){
+    delCartItem3.style.display = 'none';
+    numItem.style.display = 'none';
+};
+
+
+
+// var i;
+// var x = deleteItem1();
+// var app = document.querySelector('.app')
+
+// if (i = x){
+//     app.style.display = 'none';
+// }
